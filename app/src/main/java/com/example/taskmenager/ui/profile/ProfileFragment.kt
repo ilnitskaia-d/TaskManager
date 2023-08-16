05efe5eec360bc01d9e1570dd96b342742d3cfd9
ilnitskaia-d <ilnitskaia_d@auca.kg>
2023-08-16 23:21:25 +0600

    Initial commit

---
+++ app/src/main/java/com/example/taskmenager/ui/profile/ProfileFragment.kt
@@ -0,0 +1,25 @@
+package com.example.taskmenager.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskmenager.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
